493e2d2b7c1846dd6a5cc3e46cb620378515675c
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-12 12:56:53 -0400

    Updated dotfiles
    Automatic Configuration Script

---
+++ configure.sh
@@ -0,0 +1,24 @@
+###################################################################
+#		configure.sh
+#
+#
+#		
+#
+###################################################################
+
+
+## Instructions: Please Make sure the repository is cloned on the 
+##				 home-directory in order for this to work. 
+
+cd 
+
+cp kitty ../../.config/kitty/
+
+cp fish  ../../.config/fish/
+
+cp neofetch ../../.config/neofetch/
+
+cp micro ../../.config/micro/
+
+
+
