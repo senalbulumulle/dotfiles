5275ac3426543a701397715497767e10fb763ba1
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-18 09:50:47 -0400

    Updated dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,137 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    ########################################################
+    #				Senal's config.fish Config
+    #
+    ########################################################
+    
+    
+    
+    
+    
+    
+    ########################################################
+    #		Functions to make the workflow easier. 
+    #			
+    ########################################################
+    
+    
+    
+    
+    ########################################################
+    #		Turns on or off Hidden Files in Apple macOS
+    #		Finder
+    ########################################################
+    function mac.hidden.files.on
+    	defaults write com.apple.finder AppleShowAllFiles YES;
+    	killall Finder
+    	echo "/!\ The Hidden Files is turned on. "
+    end 
+
+    
+    function mac.hidden.files.off
+    	defaults write com.apple.Finder AppleShowAllFiles NO; 
+    	killall Finder
+    	echo "/!\ The Hidden Files is turned off. "
+    end
+
+
+
+
+
+
+
+    ########################################################
+    #    Boilerplates
+    #		
+    ########################################################
+    function boilerplate.generate.c 
+    	touch main.c
+		echo """
+/*************************************************************
+*		Title: 
+*        
+* 		Author: 
+*
+* 		Description: 
+**************************************************************/
+		
+		
+
+#include <stdio.h>
+
+int main() {
+   // Program here
+   return 0;
+}		
+
+
+	""" > main.c
+	
+    	
+    	
+    end 
+
+
+
+
+
+
+
+
+
+    
+    function boilerplate.generate.python
+        touch main.py		
+		echo """
+###############################################################
+#		Title: 
+#        
+#		Author: 
+#
+# 		Description: 
+###############################################################/
+def main():
+	## Program Here 
+    
+# Call the Main Function 
+main()		
+		
+
+
+
+
+	""" > main.py
+    	
+    	
+    end
+
+
+
+    function boilerplate.generate.html
+    touch main.html
+	echo """
+	<html>
+	  <head>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <title>Your Web Application</title>
+	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/senalbulumulle/FOIL-UI-Framework-Library/FOIL-UI.css">
+	  </head>
+	  <body>
+		<!-- Content here --> 
+	  </body>
+	</html>
+
+
+	""" > main.html    	
+    	
+    	
+    end
+
+
+
+    
     
 end
+
