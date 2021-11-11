c59c78ec48c393995c69e4064f6f92b21df60494
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-11-11 16:34:49 -0500

    Updated dotfiles

--- configure.sh
+++ configure.sh
@@ -10,11 +10,8 @@
 ## Instructions: Please Make sure the repository is cloned on the 
 ##				 home-directory in order for this to work. 
 
-cd ../.config/
 
 sudo cp -R .config ../.config
-
-cd ../ 
 
 sudo cp .bashrc ../.bashrc
 
