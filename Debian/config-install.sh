8470e4a9399d5df291d3eafad211cc67f03dfd8c
senalbulumulle <senal.bulumulle@gmail.com>
2022-01-23 21:12:32 -0500

    Added Installation for LibreWolf on my Debian Config Scripts

--- Debian/config-install.sh
+++ Debian/config-install.sh
@@ -13,6 +13,16 @@
 sudo apt update && sudo apt upgrade 
 
 
+## Installs Librewolf 
+
+## https://librewolf.net/
+## https://gitlab.com/librewolf-community
+## https://librewolf.net/installation/debian/
+
+echo "deb [arch=amd64] http://deb.librewolf.net $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+sudo apt install librewolf
+
 ## Installs Kitty
 
 ## https://sw.kovidgoyal.net/kitty/
