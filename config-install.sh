7be1ac09f19358decdf38f2c198625a08771a6a0
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-23 11:21:31 -0400

    Remodeling my config files part 13.1

--- config-install.sh
+++ config-install.sh
@@ -55,11 +55,11 @@ while :
 				##	
 				########################################################################
 
-				pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com                                                                                         && \
-				pacman-key --lsign-key FBA220DFC880C036                                                                                                                         && \        
-				pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-				cp pacman.conf /etc/pacman.conf	
-				pacman -Sy				
+				sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com                                                                                         && \
+				sudo pacman-key --lsign-key FBA220DFC880C036                                                                                                                         && \        
+				sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+				sudo cp pacman.conf /etc/pacman.conf	
+				sudo pacman -Sy				
 
 
 				## Installs Librewolf 
