786f9d3fd17083af58180c374773249059fd84bb
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-16 19:40:02 -0400

    Updated dotfiles

---
+++ Fedora/.config/polybar/Scripts/permissions.sh
@@ -0,0 +1,10 @@
+##################################################################
+##		permissions.sh
+##
+##
+##
+##	The best script to get permission to run custom
+##	scripts on polybar
+##################################################################
+
+chmod u+x title.sh
