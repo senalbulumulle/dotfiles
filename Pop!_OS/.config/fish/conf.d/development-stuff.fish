1f9ac9067ef64cf3a3f30ce39f7e47096ba12950
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-20 10:57:37 -0400

    Updated dotfiles

--- Pop!_OS/.config/fish/conf.d/development-stuff.fish
+++ Pop!_OS/.config/fish/conf.d/development-stuff.fish
@@ -1,19 +1,56 @@
 ########################################################
 #	  Development-Stuff
 ########################################################  
-function run-php-localhost:3000
-         while true
-            read -l -P '''
-                Which One? 
 
-            ''' confirm
+
+function run-php
+## Source: 
+## Modified from this source: 
+## https://stackoverflow.com/questions/16407530/how-to-get-user-confirmation-in-fish-shell
+
+         while true
+                read -l -P '''
+                        ####################################################################
+                        #                       PHP
+                        ####################################################################
+                        #
+                        #
+                        #
+                        #      Directions: 
+                        #                       After you selected the option that you
+                        #                       prefer, php will run. Press ^C to go 
+                        #                       back to the menu
+                        ####################################################################
+                        Which One? 
+
+                        a) php -S localhost:3000
+                        b) php -S 127.0.0.1:3000
+                        c) php -S localhost:4000
+                        d) php -S 127.0.0.1:4000
+                        e) Exit the Prompt 
+
+                ''' confirm
 
             switch $confirm
-              case Y y
-                return 0
-              case '' N n
-                return 1
-            end
-          end
+                        case a
+                                clear
+                                php -S localhost:3000
 
+                        case b
+                                clear
+                                php -S 127.0.0.1:3000
+
+                        case c
+                                clear
+                                php -S localhost:4000
+
+                        case d
+                                clear
+                                php -S 127.0.0.1:4000
+
+                        case e
+                                clear
+                                exit
+                end
+          end
 end
