9992cf78e6c4005ad271bf237f666c36b3cad109
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-21 13:44:41 -0400

    Updated dotfiles

--- Debian/.config/fish/conf.d/development-stuff.fish
+++ Debian/.config/fish/conf.d/development-stuff.fish
@@ -3,6 +3,12 @@
 ########################################################  
 
 
+
+
+########################################################
+#	  Options for PHP
+########################################################  
+
 function run-php
 ## Source: 
 ## Modified from this source: 
@@ -49,6 +55,83 @@ function run-php
                                 php -S 127.0.0.1:4000
 
                         case e
+                                clear
+                                return 0
+                end
+          end
+end
+
+
+
+
+
+
+
+########################################################
+#	  Generates boilerplates
+########################################################  
+
+
+function boilerplates
+## Source: 
+## Modified from this source: 
+## https://stackoverflow.com/questions/16407530/how-to-get-user-confirmation-in-fish-shell
+
+         while true
+                read -l -P '''
+                        ####################################################################
+                        #                       GENERATING A BOILERPLATE?
+                        ###################################################################
+                        
+                        Which One? 
+
+                        a) Generate C
+                        b) Generate Python
+                        c) Generate Java
+                        d) Generate HTML
+                        e) Generate README.md 
+                        f) Exit the Prompt 
+
+                ''' confirm
+
+            switch $confirm
+                        case a
+                                clear
+                                sh /home/$USER/.config/fish/conf.d/c.sh
+                                notify-send "Generated Boilerplate: [C]"
+                                micro main.c
+                                return 0
+
+                        case b
+                                clear
+                                sh /home/$USER/.config/fish/conf.d/python.sh
+                                notify-send "Generated Boilerplate: [PYTHON]"
+                                micro main.py
+                                return 0
+
+                        case c
+                                clear
+                                sh /home/$USER/.config/fish/conf.d/java.sh
+                                notify-send "Generated Boilerplate: [JAVA]"
+                                micro main.java
+                                return 0
+
+                        case d
+                                clear
+                                sh /home/$USER/.config/fish/conf.d/html.sh
+                                notify-send "Generated Boilerplate: [HTML]"
+                                micro main.html
+                                return 0
+
+
+                        case e
+                                clear
+                                sh /home/$USER/.config/fish/conf.d/readme-md.sh
+                                notify-send "Generated Boilerplate: [README.md]"
+                                micro README.md
+                                return 0
+
+                        case f
                                 clear
                                 return 0
                 end
---
+++ Gentoo/.config/fish/conf.d/readme-md.sh
@@ -0,0 +1,19 @@
+        touch README.md && echo """
+# Title
+
+
+
+#### Introduction
+
+
+#### Requirements
+
+- [ ] A
+- [ ] B
+- [ ] C
+- [ ] D
+
+
+
+#### Installation				
+        """ > README.md
