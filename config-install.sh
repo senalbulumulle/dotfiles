896d0eac6a1acf28c94d48831806c281ae8413c2
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-22 13:19:23 -0400

    Updated dotfiles

--- config-install.sh
+++ config-install.sh
@@ -22,6 +22,8 @@ echo """
 	5) macOS                                [Status: TESTED/STABLE]
 
 	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
 	7) Exit
 ========================================================================================
@@ -273,24 +275,26 @@ while :
                                 sh configure.sh
 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""
 			;;
 
@@ -588,24 +592,26 @@ while :
                                 
                                 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""
 			;;
 
@@ -835,24 +841,26 @@ while :
                                 cd Fedora && sh configure.sh
 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""
 			;;
 
@@ -1051,24 +1059,26 @@ while :
                                 cd Pop!_OS && sh configure.sh
 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""
 			;;
 
@@ -1187,24 +1197,26 @@ while :
 
 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""		
 			;;		
 
@@ -1334,26 +1346,250 @@ while :
                                 cd 
 
 				echo """
-				========================================================================================
-						Welcome to Senal's Config Installer
-				========================================================================================
-				=
-					1) Arch Linux                           [Status: STABLE]
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
 
-					2) Debian                               [Status: KINDA STABLE]
+	2) Debian                               [Status: KINDA STABLE]
 
-					3) Fedora                               [Status: STABLE]
+	3) Fedora                               [Status: STABLE]
 
-					4) Pop!_OS                              [Status: NOT TESTED]
+	4) Pop!_OS                              [Status: NOT TESTED]
 
-					5) macOS                                [Status: TESTED/STABLE]
+	5) macOS                                [Status: TESTED/STABLE]
 
-					6) Ubuntu-Multipass                     [STATUS: STABLE]
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
 
-					7) Exit
-				========================================================================================
+	7) Exit
+========================================================================================
 				"""
 			;;
+
+
+
+
+
+
+                ###########################################################
+                ##      Search: Kali-Linux
+                ###########################################################
+
+		## After the user chooses Kali-Linux, then it will install the 
+		## dependencies for my config in Kali-Linux. 
+
+	
+		4)
+				## This script installs apps/dependencies for my config and more
+
+
+				## Making sure the APT repositories/packages are updated
+				sudo apt update -y && sudo apt upgrade -y
+
+
+				## Removes the Firefox Browser 
+				sudo apt purge firefox -y
+
+
+                                ## Removes the Firefox ESR Browser 
+                                sudo apt purge firefox-esr -y
+
+
+
+				## Installs Librewolf 
+
+				## https://librewolf.net/
+				## https://gitlab.com/librewolf-community
+				## https://librewolf.net/installation/debian/
+
+				echo "deb [arch=amd64] http://deb.librewolf.net $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+				sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+				sudo apt install librewolf -y
+
+
+
+
+				## Installs Kitty
+
+				## https://sw.kovidgoyal.net/kitty/
+				## https://github.com/kovidgoyal/kitty	
+
+				sudo apt install kitty -y
+
+
+				## Installs Neofetch 
+
+				## https://github.com/dylanaraps/neofetch
+
+				sudo apt install neofetch -y
+
+
+
+				## Installs Ranger 
+
+				## https://github.com/ranger/ranger
+				## https://ranger.github.io/
+
+				sudo apt install ranger -y
+
+
+				## Installs fish 
+
+				## https://fishshell.com/
+				## https://github.com/fish-shell/fish-shell
+
+
+				sudo apt install fish -y
+
+
+
+
+				## Installs Micro Editor 
+
+				## https://micro-editor.github.io/
+				## https://github.com/zyedidia/micro
+
+				sudo apt install micro -y
+
+
+
+
+				## Installs neovim
+
+				## https://neovim.io/
+				## https://github.com/neovim
+
+				sudo apt install neovim -y
+
+
+
+
+				## Installs vim
+
+				## https://www.vim.org/
+				## https://github.com/vim
+
+				sudo apt install vim -y
+
+
+
+
+				## Installs rofi
+
+				## https://github.com/davatorium/rofi
+
+				sudo apt install rofi -y
+
+
+
+				## Installs tint2
+
+				## https://gitlab.com/o9000/tint2
+				## https://github.com/o9000/tint2
+
+
+				sudo apt install tint2 -y 
+
+
+
+
+
+				## Installs i3
+
+				## https://github.com/i3/i3
+				## https://github.com/Airblader/i3
+				## https://i3wm.org/
+
+
+				sudo apt install i3 -y 
+
+
+
+
+				## Installs OpenBox
+
+				## https://github.com/danakj/openbox
+
+
+				sudo apt install openbox -y 
+
+
+
+				## Installs Nitrogen
+
+				## https://nitrogenproject.com/
+
+				## https://github.com/nitrogen/nitrogen
+
+
+				sudo apt install nitrogen -y 
+
+
+
+				## Installs picom
+
+				## https://github.com/yshui/picom
+
+				sudo apt install picom -y 
+
+
+
+
+				## Installs Polybar
+
+				## https://github.com/polybar/polybar
+				## https://polybar.github.io/
+
+				sudo apt install polybar -y 
+
+
+				## Installs xfce4
+
+				## https://xfce.org/
+
+				## https://gitlab.xfce.org/explore/groups?sort=name_desc
+
+				sudo apt install xfce4 -y 
+
+
+				## Installs notes 
+
+				## https://github.com/pimterry/notes
+
+				curl -Ls https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
+
+
+
+                                ## Installs Configuration Files 
+                                cd Pop!_OS && sh configure.sh
+
+				echo """
+========================================================================================
+		Welcome to Senal's Config Installer
+========================================================================================
+=
+	1) Arch Linux                           [Status: STABLE]
+
+	2) Debian                               [Status: KINDA STABLE]
+
+	3) Fedora                               [Status: STABLE]
+
+	4) Pop!_OS                              [Status: NOT TESTED]
+
+	5) macOS                                [Status: TESTED/STABLE]
+
+	6) Ubuntu-Multipass                     [STATUS: STABLE]
+        
+        7) Kali-Linux                           [STATUS: NOT STABLE] [NEW]
+
+	7) Exit
+========================================================================================
+				"""
+			;;
+
 			
 
 		7)
