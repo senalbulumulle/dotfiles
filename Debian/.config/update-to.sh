7803b56f72811ae4f0c8710678a68e8e697f46d2
senalbulumulle <senal.bulumulle@gmail.com>
2022-08-22 17:15:10 -0400

    Updated dotfiles

--- Debian/.config/update-to.sh
+++ Debian/.config/update-to.sh
@@ -1,149 +1,17 @@
-cp -R awesome /home/senal/dotfiles/Arch-Linux/.config/
 cp -R awesome /home/senal/dotfiles/Debian/.config/
-cp -R awesome /home/senal/dotfiles/Fedora/.config/
-cp -R awesome /home/senal/dotfiles/Pop!_OS/.config/
-cp -R awesome /home/senal/dotfiles/Gentoo/.config/
-cp -R awesome /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R fish /home/senal/dotfiles/Arch-Linux/.config/
 cp -R fish /home/senal/dotfiles/Debian/.config/
-cp -R fish /home/senal/dotfiles/Fedora/.config/
-cp -R fish /home/senal/dotfiles/Pop!_OS/.config/
-cp -R fish /home/senal/dotfiles/Gentoo/.config/
-cp -R fish /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R helix /home/senal/dotfiles/Arch-Linux/.config/
 cp -R helix /home/senal/dotfiles/Debian/.config/
-cp -R helix /home/senal/dotfiles/Fedora/.config/
-cp -R helix /home/senal/dotfiles/Pop!_OS/.config/
-cp -R helix /home/senal/dotfiles/Gentoo/.config/
-cp -R helix /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R i3 /home/senal/dotfiles/Arch-Linux/.config/
 cp -R i3 /home/senal/dotfiles/Debian/.config/
-cp -R i3 /home/senal/dotfiles/Fedora/.config/
-cp -R i3 /home/senal/dotfiles/Pop!_OS/.config/
-cp -R i3 /home/senal/dotfiles/Gentoo/.config/
-cp -R i3 /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R kitty /home/senal/dotfiles/Arch-Linux/.config/
 cp -R kitty /home/senal/dotfiles/Debian/.config/
-cp -R kitty /home/senal/dotfiles/Fedora/.config/
-cp -R kitty /home/senal/dotfiles/Pop!_OS/.config/
-cp -R kitty /home/senal/dotfiles/Gentoo/.config/
-cp -R kitty /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R micro /home/senal/dotfiles/Arch-Linux/.config/
 cp -R micro /home/senal/dotfiles/Debian/.config/
-cp -R micro /home/senal/dotfiles/Fedora/.config/
-cp -R micro /home/senal/dotfiles/Pop!_OS/.config/
-cp -R micro /home/senal/dotfiles/Gentoo/.config/
-cp -R micro /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R neofetch /home/senal/dotfiles/Arch-Linux/.config/
 cp -R neofetch /home/senal/dotfiles/Debian/.config/
-cp -R neofetch /home/senal/dotfiles/Fedora/.config/
-cp -R neofetch /home/senal/dotfiles/Pop!_OS/.config/
-cp -R neofetch /home/senal/dotfiles/Gentoo/.config/
-cp -R neofetch /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R nitrogen /home/senal/dotfiles/Arch-Linux/.config/
 cp -R nitrogen /home/senal/dotfiles/Debian/.config/
-cp -R nitrogen /home/senal/dotfiles/Fedora/.config/
-cp -R nitrogen /home/senal/dotfiles/Pop!_OS/.config/
-cp -R nitrogen /home/senal/dotfiles/Gentoo/.config/
-cp -R nitrogen /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R nvim /home/senal/dotfiles/Arch-Linux/.config/
 cp -R nvim /home/senal/dotfiles/Debian/.config/
-cp -R nvim /home/senal/dotfiles/Fedora/.config/
-cp -R nvim /home/senal/dotfiles/Pop!_OS/.config/
-cp -R nvim /home/senal/dotfiles/Gentoo/.config/
-cp -R nvim /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R nwg-drawer /home/senal/dotfiles/Arch-Linux/.config/
 cp -R nwg-drawer /home/senal/dotfiles/Debian/.config/
-cp -R nwg-drawer /home/senal/dotfiles/Fedora/.config/
-cp -R nwg-drawer /home/senal/dotfiles/Pop!_OS/.config/
-cp -R nwg-drawer /home/senal/dotfiles/Gentoo/.config/
-cp -R nwg-drawer /home/senal/dotfiles/Kali-Linux/.config/
-
-
-cp -R nwg-launchers /home/senal/dotfiles/Arch-Linux/.config/
 cp -R nwg-launchers /home/senal/dotfiles/Debian/.config/
-cp -R nwg-launchers /home/senal/dotfiles/Fedora/.config/
-cp -R nwg-launchers /home/senal/dotfiles/Pop!_OS/.config/
-cp -R nwg-launchers /home/senal/dotfiles/Gentoo/.config/
-cp -R nwg-launchers /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R openbox /home/senal/dotfiles/Arch-Linux/.config/
 cp -R openbox /home/senal/dotfiles/Debian/.config/
-cp -R openbox /home/senal/dotfiles/Fedora/.config/
-cp -R openbox /home/senal/dotfiles/Pop!_OS/.config/
-cp -R openbox /home/senal/dotfiles/Gentoo/.config/
-cp -R openbox /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R picom /home/senal/dotfiles/Arch-Linux/.config/
 cp -R picom /home/senal/dotfiles/Debian/.config/
-cp -R picom /home/senal/dotfiles/Fedora/.config/
-cp -R picom /home/senal/dotfiles/Pop!_OS/.config/
-cp -R picom /home/senal/dotfiles/Gentoo/.config/
-cp -R picom /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R polybar /home/senal/dotfiles/Arch-Linux/.config/
 cp -R polybar /home/senal/dotfiles/Debian/.config/
-cp -R polybar /home/senal/dotfiles/Fedora/.config/
-cp -R polybar /home/senal/dotfiles/Pop!_OS/.config/
-cp -R polybar /home/senal/dotfiles/Gentoo/.config/
-cp -R polybar /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R rofi /home/senal/dotfiles/Arch-Linux/.config/
 cp -R rofi /home/senal/dotfiles/Debian/.config/
-cp -R rofi /home/senal/dotfiles/Fedora/.config/
-cp -R rofi /home/senal/dotfiles/Pop!_OS/.config/
-cp -R rofi /home/senal/dotfiles/Gentoo/.config/
-cp -R rofi /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R sway /home/senal/dotfiles/Arch-Linux/.config/
 cp -R sway /home/senal/dotfiles/Debian/.config/
-cp -R sway /home/senal/dotfiles/Fedora/.config/
-cp -R sway /home/senal/dotfiles/Pop!_OS/.config/
-cp -R sway /home/senal/dotfiles/Gentoo/.config/
-cp -R sway /home/senal/dotfiles/Kali-Linux/.config/
-
-
-
-cp -R tint2 /home/senal/dotfiles/Arch-Linux/.config/
 cp -R tint2 /home/senal/dotfiles/Debian/.config/
-cp -R tint2 /home/senal/dotfiles/Fedora/.config/
-cp -R tint2 /home/senal/dotfiles/Pop!_OS/.config/
-cp -R tint2 /home/senal/dotfiles/Gentoo/.config/
-cp -R tint2 /home/senal/dotfiles/Kali-Linux/.config/
---
+++ Ubuntu-Multipass/.config/update-to.sh
@@ -0,0 +1,6 @@
+cp -R helix /home/senal/dotfiles/Ubuntu-Multipass/.config/
+cp -R kitty /home/senal/dotfiles/Ubuntu-Multipass/.config/
+cp -R micro /home/senal/dotfiles/Ubuntu-Multipass/.config/
+cp -R neofetch /home/senal/dotfiles/Ubuntu-Multipass/.config
+cp -R nvim /home/senal/dotfiles/Ubuntu-Multipass/.config/
+
