4d32039274fd0bb0d12a41536da0414e450251d6
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-14 16:41:06 -0400

    Updated dotfiles

---
+++ Arch-Linux/install-virtualbox.sh
@@ -0,0 +1,24 @@
+## install-virtualbox.sh
+## Script to install virtualbox as well as configurating kernel modules 
+## to successfully run virtualbox
+
+function download() {
+	sudo pacman -S virtualbox
+}
+
+function configureStuff() {
+
+	cd /etc/modules-load.d/
+	sudo touch virtualbox.conf
+	sudo echo """
+	vboxdrv
+
+	""" >> virtualbox.conf
+}
+
+function reload() {
+ 	sudo modprobe
+}
+download
+configureStuff
+reload	
