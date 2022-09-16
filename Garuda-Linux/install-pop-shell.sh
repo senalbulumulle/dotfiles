8ac56776b690686d291193b8a4e37159e7cc4de5
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-16 18:31:07 -0400

    Updated dotfiles

---
+++ Garuda-Linux/install-pop-shell.sh
@@ -0,0 +1,22 @@
+## install-pop-shell.sh
+
+## Installs pop shell in Arch Linux 
+
+## Written by Senal Bulumulle
+
+
+function dependencies() {
+	sudo pacman -S git --noconfirm
+	sudo pacman -S typescript --noconfirm
+	sudo pacman -S make --noconfirm
+}
+
+function process() {
+	cd
+	git clone https://github.com/pop-os/shell.git
+	cd shell
+	make local-install
+}
+
+dependencies
+process
