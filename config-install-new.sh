884c2688215735b9a08edbb34007c14b783e0244
senalbulumulle <senal.bulumulle@gmail.com>
2022-09-16 22:15:22 -0400

    Updated dotfiles

--- config-install-new.sh
+++ config-install-new.sh
@@ -478,3 +478,479 @@ then
 	## https://github.com/kovidgoyal/kitty	
 
 	sudo apt install kitty -y
+	
+	
+	
+	
+if lsb_release -a| grep Debian
+then 
+        ## After the user chooses Debian, then it will install the 
+        ## dependencies for my config in Debian, if not, it will 
+        ## restart the script to go back to the main menu. 
+
+
+        ## Making sure the APT repositories/packages are updated
+        sudo apt update -y  && sudo apt upgrade -y
+
+
+
+        ## Installs i3
+
+        ## https://i3wm.org/
+
+        ## https://github.com/i3
+
+        sudo apt install i3 -y
+
+					        
+
+
+        ## Installs notes 
+
+        ## https://github.com/pimterry/notes
+
+        curl -Ls https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
+fi
+
+
+
+if lsb_release -a| grep Ubuntu
+then 
+        ## This script installs apps/dependencies for my config and more
+
+
+        ## Making sure the APT repositories/packages are updated
+        sudo apt update -y && sudo apt upgrade -y
+
+        ## Make sure the GNOME Desktop Environment, which is the default 
+        ## desktop environment for Ubuntu Linux, is removed from the 
+        ## System
+        sudo systemctl stop gdm
+        sudo systemctl disable gdm
+
+        ## Remove GNOME-Desktop
+        sudo apt autopurge gnome -y 
+
+        ## Remove GDM
+        sudo apt autopurge gdm -y
+
+
+        ## Make sure the lightdm is installed into the system to ensure 
+        ## user ability to log in to window manager/desktop environment
+
+        ## Install Lightdm
+        sudo apt install lightdm -y
+
+        ## Enable Lightdm
+        sudo systemctl enable lightdm
+
+        ## Removes the Firefox Browser 
+        sudo apt purge firefox -y
+
+
+        ## Removes the Firefox ESR Browser 
+        sudo apt purge firefox-esr -y
+
+
+        ## Installs Librewolf 
+
+        ## https://librewolf.net/
+        ## https://gitlab.com/librewolf-community
+        ## https://librewolf.net/installation/debian/
+
+        echo "deb [arch=amd64] http://deb.librewolf.net $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/librewolf.list
+        sudo wget https://deb.librewolf.net/keyring.gpg -O /etc/apt/trusted.gpg.d/librewolf.gpg
+        sudo apt install librewolf -y
+
+
+
+
+        ## Installs Kitty
+
+        ## https://sw.kovidgoyal.net/kitty/
+        ## https://github.com/kovidgoyal/kitty	
+
+        sudo apt install kitty -y
+
+
+        ## Installs Neofetch 
+
+        ## https://github.com/dylanaraps/neofetch
+
+        sudo apt install neofetch -y
+
+
+
+        ## Installs Ranger 
+
+        ## https://github.com/ranger/ranger
+        ## https://ranger.github.io/
+
+        sudo apt install ranger -y
+
+
+        ## Installs fish 
+
+        ## https://fishshell.com/
+        ## https://github.com/fish-shell/fish-shell
+
+
+        sudo apt install fish -y
+
+
+
+
+        ## Installs Micro Editor 
+
+        ## https://micro-editor.github.io/
+        ## https://github.com/zyedidia/micro
+
+        sudo apt install micro -y
+
+
+
+
+        ## Installs neovim
+
+        ## https://neovim.io/
+        ## https://github.com/neovim
+
+        sudo apt install neovim -y
+
+
+
+
+        ## Installs vim
+
+        ## https://www.vim.org/
+        ## https://github.com/vim
+
+        sudo apt install vim -y
+
+
+
+
+        ## Installs rofi
+
+        ## https://github.com/davatorium/rofi
+
+        sudo apt install rofi -y
+
+
+
+        ## Installs tint2
+
+        ## https://gitlab.com/o9000/tint2
+        ## https://github.com/o9000/tint2
+
+
+        sudo apt install tint2 -y 
+
+
+
+
+
+        ## Installs i3
+
+        ## https://github.com/i3/i3
+        ## https://github.com/Airblader/i3
+        ## https://i3wm.org/
+
+
+        sudo apt install i3 -y 
+
+
+
+
+        ## Installs OpenBox
+
+        ## https://github.com/danakj/openbox
+
+
+        sudo apt install openbox -y 
+
+
+
+        ## Installs Nitrogen
+
+        ## https://nitrogenproject.com/
+
+        ## https://github.com/nitrogen/nitrogen
+
+
+        sudo apt install nitrogen -y 
+
+
+
+        ## Installs picom
+
+        ## https://github.com/yshui/picom
+
+        sudo apt install picom -y 
+
+
+
+
+        ## Installs Polybar
+
+        ## https://github.com/polybar/polybar
+        ## https://polybar.github.io/
+
+        sudo apt install polybar -y 
+
+
+        ## Installs xfce4
+
+        ## https://xfce.org/
+
+        ## https://gitlab.xfce.org/explore/groups?sort=name_desc
+
+        sudo apt install xfce4 -y
+
+
+        ## Installs xfce4-terminal 
+
+        sudo apt install xfce4-terminal -y
+
+
+        ## Installs Curl
+
+        ## https://curl.se/
+
+        ## https://github.com/curl/curl
+
+        sudo apt install curl -y
+
+
+        ## Installs notes 
+
+        ## https://github.com/pimterry/notes
+
+        curl -Ls https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
+
+
+        ## Installs Make
+
+        ## https://www.gnu.org/software/make/
+
+        ## https://github.com/mirror/make
+
+        sudo apt install make -y
+
+
+
+        ## Installs Configuration Files 
+        cd Ubuntu-Linux && sh configure.sh
+            
+fi
+
+
+
+if lsb_release -a| grep Fedora
+then 
+        ## Making sure the DNF repositories/packages are updated
+        sudo dnf update && sudo dnf upgrade 
+
+        ## Enable the free repositories
+
+        ## https://docs.fedoraproject.org/en-US/quick-docs/setup_rpmfusion/
+
+        sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+        ## Enable the non-free repositories 
+
+        ## https://docs.fedoraproject.org/en-US/quick-docs/setup_rpmfusion/
+
+        sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+        ## Installs the RPM Sphere 
+
+        ## https://rpmsphere.github.io/
+
+        ## https://github.com/rpmsphere
+
+        wget https://github.com/rpmsphere/noarch/raw/master/r/rpmsphere-release-36-1.noarch.rpm
+        sudo rpm -i -y rpmsphere-release-36.1.noarch.rpm
+
+
+        ## Installs MarkText
+
+        ## https://github.com/marktext/marktext
+
+        ## https://marktext.app/
+
+        wget https://github.com/marktext/marktext/releases/download/v0.17.1/marktext-x86_64.rpm
+        sudo rpm -i -y marktext-x86_64.rpm
+
+
+        ## Installs Kitty
+
+        ## https://sw.kovidgoyal.net/kitty/
+        ## https://github.com/kovidgoyal/kitty	
+
+        sudo dnf -y install kitty
+
+
+        ## Installs Neofetch 
+
+        ## https://github.com/dylanaraps/neofetch
+
+        sudo dnf -y install neofetch
+
+
+        ## Removes Firefox 
+
+        sudo dnf -y erase firefox
+
+        ## Installs Ranger 
+
+        ## https://github.com/ranger/ranger
+        ## https://ranger.github.io/
+
+        sudo dnf -y install ranger
+
+
+        ## Installs fish 
+
+        ## https://fishshell.com/
+        ## https://github.com/fish-shell/fish-shell
+
+
+        sudo dnf -y install fish
+
+
+
+
+        ## Installs Micro Editor 
+
+        ## https://micro-editor.github.io/
+        ## https://github.com/zyedidia/micro
+
+        sudo dnf -y install micro
+
+
+
+
+        ## Installs neovim
+
+        ## https://neovim.io/
+        ## https://github.com/neovim
+
+        sudo dnf -y install neovim
+
+
+
+
+        ## Installs vim
+
+        ## https://www.vim.org/
+        ## https://github.com/vim
+
+        sudo dnf -y install vim
+
+
+
+
+
+
+
+
+        ## Installs OpenBox
+
+        ## https://github.com/danakj/openbox
+
+
+        sudo dnf -y install openbox
+
+
+
+
+
+
+
+        ## Installs Nitrogen
+
+        ## https://nitrogenproject.com/
+
+        ## https://github.com/nitrogen/nitrogen
+
+
+        sudo dnf -y install nitrogen
+
+
+
+        ## Installs Librewolf
+
+        ## https://librewolf.net/
+
+        ## https://gitlab.com/librewolf-community
+
+        ## https://librewolf.net/installation/fedora/
+
+
+        sudo rpm --import https://keys.openpgp.org/vks/v1/by-fingerprint/034F7776EF5E0C613D2F7934D29FBD5F93C0CFC3
+
+        sudo dnf config-manager --add-repo https://rpm.librewolf.net
+
+        sudo dnf update
+
+        sudo dnf -y install librewolf
+
+
+
+
+        ## Installs picom
+
+        ## https://github.com/yshui/picom
+
+        sudo dnf -y install picom
+
+
+
+        ## Installs Polybar
+
+        ## https://github.com/polybar/polybar
+        ## https://polybar.github.io/
+
+        sudo dnf -y install polybar
+
+
+        ## Installs xfce4
+
+        ## https://xfce.org/
+
+        ## https://gitlab.xfce.org/explore/groups?sort=name_desc
+
+        sudo dnf -y install xfce4
+
+
+        ## Installs i3
+
+        ## https://i3wm.org/
+
+        ## https://github.com/i3
+
+        sudo dnf -y install i3
+
+
+
+        ## Installs brightnessctl
+
+        ## https://github.com/Hummer12007/brightnessctl
+
+        sudo dnf -y install brightnessctl
+
+
+        ## Installs notes 
+
+        ## https://github.com/pimterry/notes
+
+        curl -Ls https://raw.githubusercontent.com/pimterry/notes/latest-release/install.sh | sudo bash
+
+
+
+
+        ## Installs Configuration Files 
+        cd Fedora && sh configure.sh
+fi
