63b0dda010062ed72e6f941cbc6597ec3db21497
senalbulumulle <senal.bulumulle@gmail.com>
2022-10-07 17:28:53 -0400

    Updated dotfiles

---
+++ xubuntu/.config/fish/conf.d/java.sh
@@ -0,0 +1,17 @@
+        touch main.java && echo """
+/**************************************************************
+*	Title: 
+*	
+*	Author:
+*
+*	Description:
+*
+***************************************************************/
+public class Main {
+	
+	public static void main(String[] args) {
+
+		// Program Here
+	}
+}				
+        """ > main.java
