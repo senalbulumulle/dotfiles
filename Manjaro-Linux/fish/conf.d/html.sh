bfcbef7fc44f3b9051a9fc70935143b8bb418472
senalbulumulle <senal.bulumulle@@gmail.com>
2022-11-30 09:43:59 -0500

    Updated dotfiles

---
+++ Manjaro-Linux/fish/conf.d/html.sh
@@ -0,0 +1,13 @@
+        touch main.html && echo """
+<html>
+        <head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Your Web Application</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/senalbulumulle/FOIL-UI-Framework-Library/FOIL-UI.css">
+        </head>
+
+        <body>
+                <!-- Content here --> 
+        </body>
+</html>					
+        """ > main.html
