12a248fa04483e70d5508a114231c5f16280e9f3
senalbulumulle <senal.bulumulle@gmail.com>
2023-02-20 13:58:43 -0500

    Updated dotfiles

--- archlinuxsetup.sh
+++ archlinuxsetup.sh
@@ -8,11 +8,17 @@
 
 sudo pacman -S --noconfirm i3
 sudo pacman -S --noconfirm i3lock i3status dmenu feh xorg-xrandr
+sudo pacman -S --noconfirm ranger --noconfirm
+sudo pacman -S --noconfirm tint2 --noconfirm
+sudo pacman -S --noconfirm kitty --no-confirm 
+sudo pacman -S --noconfirm alacritty --noconfirm 
+sudo pacman -S discord --noconfirm
+sudo pacman -S gnome-software --noconfirm
+sudo pacman -S power-profiles-daemon
+yay -S librewolf-bin --noconfirm
 sudo pacman -S gdm --noconfirm
 sudo systemctl enable gdm
-
+sudo systemctl enable power-profiles-daemon
 sudo gpasswd -a $USER video
-
-
 sudo systemctl reboot
 
