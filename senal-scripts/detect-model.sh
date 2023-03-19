37a3e5804b4134d4d7ec066bbf825f0bae5bdade
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-19 11:28:34 -0400

    Updated dotfiles

--- senal-scripts/detect-model.sh
+++ senal-scripts/detect-model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Get the System76 model name
+# Get the Hardware model name
 model=$(sudo dmidecode -s system-product-name)
 
 # Get the firmware version
 firmware=$(sudo dmidecode -s bios-version)
 
-# Print the System76 model and firmware version
-echo "System76 Model: $model"
+# Print the hardware model and firmware version
+echo "Hardware Model: $model"
 echo "Firmware Version: $firmware"
