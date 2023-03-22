da605d5b5558cc48787d48709208875511160230
senalbulumulle <senal.bulumulle@gmail.com>
2023-03-22 12:00:24 -0400

    Updated dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
+        ## Set default editor
 
+        ## For example if you want vim, replace "micro" with vim
+        ## For example if you want nvim, replace "micro" with nvim
+        
+        export EDITOR="micro"   
+        export VISUAL="micro"
+
+        
         ## If the user types in 'detect-model', it will prompt the user 
         ## the superuser password, before showing the model name, and
         ## the firmware version of the hardware. 
         
         function detect-model
                 sh /home/$USER/.config/senal-scripts/detect-model.sh
-
         end      
         
 end
