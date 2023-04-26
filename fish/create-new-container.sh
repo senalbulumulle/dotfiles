65963fd896ebdecbd5a1eea29c714f5ac9fbd146
senalbulumulle <senal.bulumulle@gmail.com>
2023-04-26 10:08:56 -0400

    Updated dotfiles

--- fish/create-new-container.sh
+++ fish/create-new-container.sh
@@ -4,16 +4,16 @@
 #	  Create containers more easily on distrobox
 #########################################################
 
-function createContainer() {
+function createNewContainer() {
 	clear
 
-	echo "Please name the new container for Arch Linux"
+	echo "Please name the new container **Default Fedora**"
 
 	read name
 
-	distrobox create --clone Arch-Linux-Template --name $name
+	distrobox create --name $name
 	distrobox enter $name
 		
 }
 
-createContainer
+createNewContainer
