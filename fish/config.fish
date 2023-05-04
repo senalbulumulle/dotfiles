ac3c95ea74f9a0f06e2af1f786af72e3996315ea
senalbulumulle <senal.bulumulle@gmail.com>
2023-05-04 07:02:24 -0400

    Updated dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -16,14 +16,17 @@ if status is-interactive
 
 
         ## ALIASES
-        alias emacsnw='emacs --no-window-system'                                ## Launches Senal's Emacs in the terminal instead of the window
-	alias ce='distrobox enter'                                              ## This will enter to a distrobox container
-	alias cs='distrobox stop'                                               ## This will stop a distrobox container
-        alias cm='distrobox rm'                                                 ## This will remove a distrobox container
-	alias bashedit='micro /home/$USER/.bashrc'                              ## Goes directly to [/home/$USER/.bashrc]                                         
-        alias about='clear && cat /home/$USER/.config/fish/about'               ## Shows the about screen about Senal's Config
-        alias manual='less /home/$USER/dotfiles/README.org'                     ## Shows the manual how to do things with Senal's config in general	
+        alias emacss='emacs --no-window-system'                                         ## Launches Senal's Emacs in the terminal instead of the window
+	alias ce='distrobox enter'                                                      ## This will enter to a distrobox container
+	alias cs='distrobox stop'                                                       ## This will stop a distrobox container
+        alias cm='distrobox rm'                                                         ## This will remove a distrobox container
+        alias cl='clear && distrobox rm'                                                ## This will remove a distrobox container
+	alias bashedit='micro /home/$USER/.bashrc'                                      ## Goes directly to [/home/$USER/.bashrc]                                         
+        alias about='clear && cat /home/$USER/.config/fish/about'                       ## Shows the about screen about Senal's Config
+        alias manual='less /home/$USER/dotfiles/README.org'                             ## Shows the manual how to do things with Senal's config in general	
+        alias kittyconfig='micro /home/$USER/.config/kitty/kitty.conf'                  ## Goes directory to [/home/$USER/.config/kitty/kitty.conf]
 
+        
         ## If the user types in 'detect-model', it will prompt the user         
         ## the superuser password, before showing the model name, and
         ## the firmware version of the hardware. 
@@ -40,31 +43,4 @@ if status is-interactive
 	function config-fish 
 	        micro /home/$USER/.config/fish/config.fish
 	end
-
-	## CONTAINER NEW CLONE
-	function cnc
-                bash /home/$USER/.config/fish/create-container.sh
-	end
-
-
-	function cn
-                bash /home/$USER/.config/fish/create-new-container.sh
-	end
-
-	## CONTAINER CLONE EDIT
-	function cce
-		micro /home/$USER/.config/fish/container-clone-edit.sh
-	end
-
-	## CONTAINER IMAGE REFERENCE 
-	function ccr
-		clear
-		cat /home/$USER/clone-image-reference
-	end
-
-	## LIST CONTAINERS 
-	function cl
-		clear
-		distrobox list
-	end 
 end
