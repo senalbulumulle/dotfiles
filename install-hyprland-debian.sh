6b4df7171d7534aa1596fa3a6bc222e5d84cd0c8
senalbulumulle <senal.bulumulle@gmail.com>
2023-11-30 10:39:31 -0500

    Updated dotfiles

--- install-hyprland-debian.sh
+++ install-hyprland-debian.sh
@@ -1,9 +1,2 @@
 ## install-hyprland-debian.sh
 
-sudo apt-get install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr
-
-cd /home/$USER
-
-git clone --recursive https://github.com/hyprwm/Hyprland
-cd Hyprland
-make all && sudo make install
