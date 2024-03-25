25d09a4b0a9eba055fc6f6928da07074fdfdeed8
senalbulumulle <senal.bulumulle@gmail.com>
2024-03-25 00:22:40 -0700

    Updated dotfiles

---
+++ push.ps1
@@ -0,0 +1,10 @@
+###############################################
+#		push.ps1
+###############################################
+
+
+git add *
+
+git commit -m "Updated dotfiles"
+
+git push origin main
