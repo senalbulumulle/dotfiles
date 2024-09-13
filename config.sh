f19795cce4a7e93f0d6102e48697ce8f48e81bfe
senalbulumulle <senal.bulumulle@gmail.com>
2024-09-13 10:30:02 -0400

    Updated dotfiles

--- config.sh
+++ config.sh
@@ -1,19 +1,24 @@
 ## config.sh
 
 
-function fish_config()
-{
-	
-}
+
+rm -rf /home/$USER/.config/fish/config.fish
+echo """
+## Senal's Fish config for macOS and Linux 
+
+if status is-interactive
+    alias microconfig='micro $HOME/.config/micro/settings.json'
+    alias vimconfig='vim $HOME/.vimrc'
+    alias bashconfig='micro $HOME/.bashrc'
+    alias fishconfig='micro $HOME/.config/fish/config.fish'
+    alias zshconfig='micro $HOME/.zshrc'
+    alias neovideconfig='echo "not available yet"'
+    alias v='nvim'
+    alias m='micro'
+    alias markdown='open /Applications/MarkText.app/'
+    alias l='ls -l'
+    alias ll='ls -la'
+end
 
 
-function micro_config()
-{
-	
-}
-
-
-function kitty_config()
-{
-	
-}
+"""	>> /home/$USER/.config/fish/config.fish
