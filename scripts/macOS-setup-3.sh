de30008be1732170e397e117218c4a1bff1d1a20
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-28 15:16:44 -0500

    Added scripts/ [macOS-setup-3.sh]

---
+++ scripts/macOS-setup-3.sh
@@ -0,0 +1,11 @@
+## Create the mkdir -p ~/.local/bin directory
+mkdir -p ~/.local/bin
+
+
+
+cd /Users/$USER/.local/bin && touch update-apps.sh			## Allows the ability to install, and delete dependencies
+															## dynamically
+
+
+
+cd /Users/$USER/.local/bin && touch update-brew.sh			## Allows the ability to update and upgrade brew packages
