3fbe695a000362e1e49e013661076de9d19a5b96
senalbulumulle <senal.bulumulle@gmail.com>
2024-12-28 14:00:43 -0500

    updated the script

--- compile-underscore.sh
+++ compile-underscore.sh
@@ -1,7 +1,11 @@
 ## compile-underscore.sh
 
 cp -R _fish fish
-cp -R _hypr hypr
 cp -R _i3 i3
 cp -R _sway sway
 cp -R _kitty kitty
+
+
+## Then compile the thing to the .config directory
+mv fish /Users/$USER/.config/
+mv kitty /Users/$USER/.config/
