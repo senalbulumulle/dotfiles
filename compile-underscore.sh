1261dfb03f6380f28acf28daf9f2299c2f19eefd
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-02 13:16:14 -0500

    removed [compile-underscore.sh]

--- compile-underscore.sh
+++
@@ -1,9 +0,0 @@
-## compile-underscore.sh
-
-cp -R _fish fish
-cp -R _kitty kitty
-
-
-## Then compile the thing to the .config directory
-mv fish /Users/$USER/.config/
-mv kitty /Users/$USER/.config/
