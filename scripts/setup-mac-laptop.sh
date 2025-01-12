a6791f9ad51797166948f0e91a67dd49e153df40
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-12 12:31:27 -0500

    updated dot

--- scripts/setup-mac-laptop.sh
+++ scripts/setup-mac-laptop.sh
@@ -1,20 +1,37 @@
-brew install discord
-brew install fish
-brew install microsoft-edge@dev
-brew install google-chrome
-brew install firefox
-brew uninstall vscodium
-brew install gh
-brew install kitty
-brew install micro
-brew install signal
-brew install watchman
-brew install nvim
-brew install php
-brew install tabby
-brew install chatgpt
-brew uninstall firefox
-brew install opera-gx
-brew install macs-fan-control
-brew install zen-browser
-brew install code-server
+## setup-mac-laptop
+
+function uninstall() {
+    brew uninstall vscodium    
+    brew uninstall firefox
+    brew uninstall opera-gx
+    brew uninstall kitty
+    brew uninstall tabby
+}
+uninstall
+
+function install() {
+    brew install discord
+    brew install fish
+    brew install microsoft-edge@dev
+    brew install google-chrome
+    brew install gh
+    brew install micro
+    brew install signal
+    brew install watchman
+    brew install nvim
+    brew install php
+    brew install chatgpt
+    brew install macs-fan-control
+    brew install zen-browser
+    brew install code-server
+    brew install ghostty
+}
+install
+
+
+
+function setup()
+{
+    cd ../setup-mac-laptop-config && cp -R fish /Users/senal/.config/ && cp -R micro /Users/senal/.config/
+}
+setup
