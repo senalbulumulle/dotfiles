14f82c077ed03ffdd4e0c0f18305c7fe02e97d7b
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-12 12:31:58 -0500

    updated dot

---
+++ setup-mac-laptop-config/fish/config.fish
@@ -0,0 +1,13 @@
+## Senal's Fish config for macOS and Linux 
+
+if status is-interactive
+    alias microconfig='micro $HOME/.config/micro/settings.json'
+    alias vimconfig='vim $HOME/.vimrc'
+    alias bashconfig='micro $HOME/.bashrc'
+    alias fishconfig='micro $HOME/.config/fish/config.fish'
+    alias zshconfig='micro $HOME/.zshrc'
+    alias neovideconfig='echo "not available yet"'
+    alias v='nvim'
+    alias m='micro'
+    alias markdown='open /Applications/MarkText.app/'
+end
