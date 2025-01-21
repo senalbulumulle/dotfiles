275c123b4a04292c8abd47b60d5ae9aaae34d87a
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-21 10:58:34 -0500

    okay that works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/**
+Senal's dotfiles manager
+Written By Senal Bulumule 
+**/
+
+
+
+// This is where we are able to Import Libraries
+
+extern crate symlink;
+use symlink::{symlink_auto, symlink_file, symlink_dir, remove_symlink_file, remove_symlink_dir};
+
+
+
 fn main() {
-    println!("Hello, world!");
+	
 }
