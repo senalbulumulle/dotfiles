4b0e9a398bc1d9224cc151133d30b2ab4b56406b
senalbulumulle <senal.bulumulle@gmail.com>
2025-01-24 10:20:59 -0500

    documentated code lol

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,39 @@
 /**
 Senal's dotfiles manager
-Written By Senal Bulumule 
+Written By Senal Bulumulle 
 **/
 
 
 
 // This is where we are able to Import Libraries
 
-extern crate symlink;
-use symlink::{symlink_auto, symlink_file, symlink_dir, remove_symlink_file, remove_symlink_dir};
+
+use std::process::{Command, exit};
 
 
+
+// This is a function where you are able to symlink the vimrc file
+fn symlink_vimrc()
+{
+    let _output = Command::new("ln -s /Users/$USER/dotfiles/.vimrc /Users/$USER/.vimrc").output();
+}
+
+
+// This is a function where you are able to symlink the zshrc file
+fn symlink_zshrc()
+{
+    let _output = Command::new("ln -s /Users/$USER/dotfiles/.zshrc /Users/$USER/.zshrc").output();
+}
+
+// This is a function where you are able to symlink the aerospacerc file
+fn symlink_aerospacerc()
+{
+    let _output = Command::new("ln -s /Users/$USER/dotfiles/.aerospace.toml /Users/$USER/.aerospace.toml").output();
+}
 
 fn main() {
-	
+    println!("hello world");
+    symlink_vimrc();
+    symlink_zshrc();
+    symlink_aerospacerc();
 }
