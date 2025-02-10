ccfceceea86fd85707c4be22fb7084609599a4a1
senalbulumulle <senal.bulumulle@gmail.com>
2025-02-10 18:55:05 -0500

    Updated dotfiles

---
+++ scripts/ThinkPad-T480/01.sh
@@ -0,0 +1,31 @@
+## setup-laptop-thinkpad-t480
+## setup minimal setup 
+
+
+function updatePackages()
+{
+	sudo pacman -Syyu --noconfirm
+}
+updatePackages
+
+
+function uninstallApps()
+{
+	echo "none"
+}
+
+
+function installApps()
+{
+	sudo pacman -S neovim --noconfirm
+    sudo pacman -S i3 --noconfirm
+    sudo pacman -S dmenu --noconfirm
+    sudo pacman -S emacs --noconfirm
+    sudo pacman -S github-cli --noconfirm
+}
+
+
+function enableServices()
+{
+	echo "enable services"	
+}
