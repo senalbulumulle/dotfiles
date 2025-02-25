d68f77bea9e4b91be0f72c240bfd263c8df3c495
senalbulumulle <senal.bulumulle@gmail.com>
2025-02-25 12:08:37 -0500

    Documentated the keybindings on the nvim config more...

--- nvim/lua/senal/keybindings.lua
+++ nvim/lua/senal/keybindings.lua
@@ -7,4 +7,6 @@ vim.g.mapleader = " "
 
 
 -- Gets into the file manager vim file manager. This may change in the future 
+-- How you able to do that, just simply use <space> and then pv to go the file
+-- manager
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
