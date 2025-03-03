3d7a7589bbe5bf10b438ed1e286075583c340ac3
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-03 13:54:19 -0500

    Updated dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,4 +6,13 @@ require("senal.keybindings")
 require("senal.lazy")
 print("hello world")
 
+
+shiftwidth = 2;
+softtabstop = -1;
+
+
+vim.opt.tabstop = 8       -- Set the width of a tab character
+vim.opt.shiftwidth = 8    -- Set the number of spaces to use for each step of indentation
+vim.opt.expandtab = true  -- Use spaces instead of tabs
+
 return {}
