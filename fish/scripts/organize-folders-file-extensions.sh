e643cbdd597779b8535ab6736fd1c510b13b2032
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-04 07:54:51 -0500

    Added script: [fish/scripts/organize-folders-file-extensions.sh]

---
+++ fish/scripts/organize-folders-file-extensions.sh
@@ -0,0 +1,21 @@
+mkdir svg
+mkdir iso
+mkdir mp3
+mkdir jpg
+mkdir dmg
+mkdir jpeg
+mkdir zip
+mkdir pdf
+
+
+
+
+
+mv *.svg svg
+mv *.iso iso
+mv *.mp3 mp3
+mv *.jpg jpg
+mv *.dmg dmg
+mv *.jpeg jpeg
+mv *.zip zip
+mv *.pdf pdf
