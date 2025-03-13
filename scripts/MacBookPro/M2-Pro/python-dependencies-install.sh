6f599a0d2d064dd8bb1ea6ad29b50bd9febf8ee9
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-13 12:30:23 -0400

    Updated: [scripts/MacBookPro/

--- scripts/MacBookPro/M2-Pro/python-dependencies-install.sh
+++ scripts/MacBookPro/M2-Pro/python-dependencies-install.sh
@@ -2,7 +2,7 @@
 
 function install() {
 	pip3 install flask --break-system-packages
-	pip3 install ollama-python --break-system-packages
+	pip3 install ollama --break-system-packages
 	pip3 install pytest --break-system-packages
 	
 }
