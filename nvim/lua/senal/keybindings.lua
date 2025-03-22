6e0e7d8323a314a3c8363ebe5ba371772929fb3d
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-22 00:20:06 -0400

    updated keybindings

--- nvim/lua/senal/keybindings.lua
+++ nvim/lua/senal/keybindings.lua
@@ -10,3 +10,7 @@ vim.g.mapleader = " "
 -- How you able to do that, just simply use <space> and then pv to go the file
 -- manager
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Allows the ability to indent the code --
+vim.keymap.set("n", "<leader>is", "=", { desc = "Indent selected block" })
