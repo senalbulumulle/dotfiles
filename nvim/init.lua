ba3a0c05c09ea6c5107b27b85278c325d88972e3
senalbulumulle <senal.bulumulle@gmail.com>
2025-03-26 12:14:08 -0400

    mmm

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,8 +7,8 @@ shiftwidth = 2;
 softtabstop = -1;
 
 
-vim.opt.tabstop = 4       -- Set the width of a tab character
-vim.opt.shiftwidth = 4    -- Set the number of spaces to use for each step of indentation
+vim.opt.tabstop = 8       -- Set the width of a tab character
+vim.opt.shiftwidth = 8    -- Set the number of spaces to use for each step of indentation
 vim.opt.expandtab = true  -- Use spaces instead of tabs
 
 return {}
