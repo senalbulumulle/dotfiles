5cb5b5097130d3d63049165cc8d08d3575108dbb
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-07 12:00:53 +0530

    Make it to 8 spaces and 8 tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,8 +17,8 @@ softtabstop = -1;
 -- 
 --
 
-vim.opt.tabstop = 4       -- Set the width of a tab character
-vim.opt.shiftwidth = 4    -- Set the number of spaces to use for each step of indentation
+vim.opt.tabstop = 8       -- Set the width of a tab character
+vim.opt.shiftwidth = 8    -- Set the number of spaces to use for each step of indentation
 vim.opt.expandtab = true  -- Use spaces instead of tabs
 
 return {}
