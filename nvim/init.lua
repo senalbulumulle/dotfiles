135100a9f83560cb8a1a5304ebd358cd78c037e5
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-12 07:19:31 +0530

    mmm

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,7 @@
--- Senal's Neovim Config --
+------------------------------------------------------
+--              SENAL NEOVIM CONFIG
+--
+------------------------------------------------------
 
 -- Requiring files here --
 require("senal.keybindings")
@@ -8,17 +11,15 @@ require("senal.kickstart")
 -- Require Files for custom programs in neovim --
 -- require custom programs here --
 
+shiftwidth = 2
+softtabstop = -1
 
-shiftwidth = 2;
-softtabstop = -1;
-
-
--- 
--- 
+--
+--
 --
 
-vim.opt.tabstop = 8       -- Set the width of a tab character
-vim.opt.shiftwidth = 8    -- Set the number of spaces to use for each step of indentation
-vim.opt.expandtab = true  -- Use spaces instead of tabs
+vim.opt.tabstop = 8 -- Set the width of a tab character
+vim.opt.shiftwidth = 8 -- Set the number of spaces to use for each step of indentation
+vim.opt.expandtab = true -- Use spaces instead of tabs
 
 return {}
