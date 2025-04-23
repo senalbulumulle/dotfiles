98d68be56cd6a30149fd5ab1ff7fcfc4b3dee41f
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-23 17:37:54 -0400

    changed the directory to .local/share/nvim/site/pack/manual/start/ for nvim

--- nvim/init-install.sh
+++ nvim/init-install.sh
@@ -4,8 +4,8 @@
 
 function downloadRepo()
 {
-	mkdir -p /Users/$USER/.local/share/nvim
-	cd /Users/$USER/.local/share/nvim
+	mkdir -p /Users/$USER/.local/share/nvim/site/pack/manual/start/
+	cd /Users/$USER/.local/share/nvim/site/pack/manual/start/
         git clone https://github.com/stevearc/aerial.nvim
         git clone https://github.com/rcarriga/nvim-dap-ui
         git clone https://github.com/folke/which-key.nvim
