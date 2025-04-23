84785fd92a42da60313d814f5a2af792580ddaa0
senalbulumulle <senal.bulumulle@gmail.com>
2025-04-23 13:11:22 -0400

    documentated the code

--- nvim/lua/senal/otherkeybindings.lua
+++ nvim/lua/senal/otherkeybindings.lua
@@ -1,7 +1,6 @@
 -- other keybindings --
--- Based on the plugins that are downloaded the other keybindings represent that -- 
-
-
+-- Based on the plugins that are downloaded the other keybindings represent that --
+-- the plugins that are included in here are located in  ~/.local/share/nvim/site/pack/manual/start/--
 
 
 
