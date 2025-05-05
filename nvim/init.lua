be370a63548404feedb6684f0cbc1b990feaae64
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-05 12:05:25 -0400

    updated nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,19 +6,20 @@
 -- Requiring files here --
 require("senal.keybindings")
 require("senal.otherkeybindings")
-
--- Require Files for custom programs in neovim --
--- require custom programs here --
+require("senal.customcommands")
+require("senal.customui")
 
 shiftwidth = 2
 softtabstop = -1
 
---
---
---
+
 
 vim.opt.tabstop = 8 -- Set the width of a tab character
 vim.opt.shiftwidth = 8 -- Set the number of spaces to use for each step of indentation
 vim.opt.expandtab = true -- Use spaces instead of tabs
+
+
+-- prevent wrapping issues --
+
 
 return {}
