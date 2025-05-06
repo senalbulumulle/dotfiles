2ce69915fd4f976d12c1d02df95877cefe1c5512
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-06 11:57:19 -0400

    updated nvim

---
+++ nvim/update-install.sh
@@ -0,0 +1,24 @@
+## init-install.sh
+
+
+
+function downloadRepo()
+{
+	cd /Users/$USER/.local/share/nvim/site/pack/manual/start/
+	cd aerial.nvim && git pull && cd ../
+	cd nui.nvim && git pull && cd ../
+	cd nvim-dap-ui && git pull && cd ../
+	cd telescope.nvim && git pull && cd ../
+	cd which-key.nvim && git pull && cd ../
+}
+
+downloadRepo
+
+
+
+function checkRepo()
+{
+
+	echo "repos are updated"
+}
+checkRepo
