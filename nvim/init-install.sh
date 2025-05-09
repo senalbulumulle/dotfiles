7f8444fab57abb8487e0f9b7828b1214269861b7
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-09 11:44:48 -0400

    Added installation of other plugins to neovim

--- nvim/init-install.sh
+++ nvim/init-install.sh
@@ -1,7 +1,5 @@
 ## init-install.sh
 
-
-
 function downloadRepo()
 {
 	mkdir -p /Users/$USER/.local/share/nvim/site/pack/manual/start/
@@ -11,16 +9,17 @@ function downloadRepo()
         git clone https://github.com/folke/which-key.nvim
         git clone https://github.com/MunifTanjim/nui.nvim
         git clone https://github.com/nvim-telescope/telescope.nvim
-        git clone https://github.com/MunifTanjim/nui.nvim
-
+        git clone https://github.com/github/copilot.vim
+        git clone https://github.com/neoclide/coc.nvim
+        git clone https://github.com/hrsh7th/nvim-cmp
+        git clone https://github.com/nvim-treesitter/nvim-treesitter
+        git clone https://github.com/glepnir/lspsaga.nvim
 }
+
 downloadRepo
-
-
 
 function checkRepo()
 {
-
 	echo "checking repos installed"
 }
 checkRepo
