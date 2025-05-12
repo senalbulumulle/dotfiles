931573caf1afb39a1db5ec06e864b08e50dd7526
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-12 03:10:20 -0400

    make it minimal

--- NvChad/lua/chadrc.lua
+++ NvChad/lua/chadrc.lua
@@ -5,14 +5,14 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
-}
+-- M.base46 = {
+-- 	theme = "onedark",
+--
+-- 	-- hl_override = {
+-- 	-- 	Comment = { italic = true },
+-- 	-- 	["@comment"] = { italic = true },
+-- 	-- },
+-- }
 
 -- M.nvdash = { load_on_startup = true }
 -- M.ui = {
