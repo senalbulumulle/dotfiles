2d61c1b0653443d579662bf66ec79efc8831b27f
senalbulumulle <senal.bulumulle@gmail.com>
2025-05-31 14:19:38 -0400

    Disabled tokyonight.nvim plugin

--- kickstart.nvim/init.lua
+++ kickstart.nvim/init.lua
@@ -756,23 +756,23 @@ require('lazy').setup({
     },
   },
 
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+--   {
+--     'folke/tokyonight.nvim',
+--     priority = 1000, -- Make sure to load this before all the other start plugins.
+--     config = function()
+--       ---@diagnostic disable-next-line: missing-fields
+--       require('tokyonight').setup {
+--         styles = {
+--           comments = { italic = false }, -- Disable italics in comments
+--         },
+--       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+--       -- Load the colorscheme here.
+--       -- Like many other themes, this one has different styles, and you could load
+--       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+--       vim.cmd.colorscheme 'tokyonight-night'
+--     end,
+--   },
 
   {
     'folke/todo-comments.nvim',
