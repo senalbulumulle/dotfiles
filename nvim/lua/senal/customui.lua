7d8106381e91504bfb86f9ff286967a0dcc4539e
senalbulumulle <senal.bulumulle@gmail.com>
2025-06-12 09:22:19 -0700

    No need of neovim configs here since senalvim (neovim config) is there...... lol

--- nvim/lua/senal/customui.lua
+++
@@ -1,5 +0,0 @@
--- customui --
-
--- This is where we are able to require the nui.nvim library
-local Popup = require("nui.popup")
-local Layout = require("nui.layout")
--- nvim/lua/senal/otherkeybindings.lua
+++
@@ -1,9 +0,0 @@
--- other keybindings --
--- Based on the plugins that are downloaded the other keybindings represent that --
--- the plugins that are included in here are located in  ~/.local/share/nvim/site/pack/manual/start/--
-
-
-
-
--- Aerial setup, this is where we are able to require the aerial
-require("aerial")
