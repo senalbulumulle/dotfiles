b4595bcb066bb620c859187d657dbd270de4e8a0
senalbulumulle <senal.bulumulle@gmail.com>
2025-10-05 17:57:25 -0400

    mmm

--- setup.sh
+++ setup.sh
@@ -1,89 +1,47 @@
-## setup.sh
-
-## This is where we make a function called uninstall() which uninstalls the packages via Brew
-function uninstall() {
-	brew uninstall --cask karabiner-elements
-	brew uninstall firefox
-	brew uninstall opera-gx
-	brew uninstall tabby
-	brew uninstall microsoft-edge@dev
-	brew uninstall ghostty
-	brew uninstall gpt4all
-	brew uninstall zellij
-	brew uninstall blender
-	brew uninstall aerospace
-	brew uninstall wacom-tablet
-	brew uninstall emacs
-	brew uninstall ollama
-	brew uninstall vscodium
-	brew uninstall vscodium@insiders
-	brew uninstall loop
-	brew uninstall visual-studio-code
-	brew uninstall code-server
-	brew uninstall hugo
-	brew uninstall deno
-	brew uninstall flutter
-	brew uninstall alt-tab
-	brew uninstall sdl2 sdl2_ttf
-	brew uninstall mactex
-
-
-	## --- Machine Learning Tools -- ##
-
-
-}
-uninstall
-
-## This is where we make a function called installCask which installs the cask packages via Brew
-function installCask() {
+function installCask() 
+{
 	brew install --cask cursor
 	brew install --cask emacs
 	brew install --cask neovide
 	brew install --cask ollama
 	brew install --cask ranger
+  brew install --cask discord
+  brew install --cask signal
+  brew install --cask alacritty
+  brew install --cask ghostty
+  brew install --cask rustdesk
 	brew install --cask godot
 	brew install --cask streamlabs
+  brew install --cask libreoffice
 	brew install --cask obs
 	brew install --cask webull
 	brew install --cask microsoft-teams
 	brew install --cask utm
 	brew install --cask parallels
 	brew install --cask obsidian
-
-
-	## --- Machine Learning Tools -- ##
-
-
 	brew install --cask anaconda
-
-
-
-
+  brew install --cask google-chrome
+  brew install --cask zen-browser
+  brew install --cask opera-air
+  brew install --cask vivaldi
+  brew install --cask arduino-ide
+  brew install --cask utm
+  brew install --cask macs-fan-control
 }
 installCask
 
 
-## This is where we make a function called install which installs the packages via Brew
+
 function install() {
 	brew install qemu
-	brew install discord
 	brew install fish
-	brew install google-chrome
 	brew install gh
 	brew install micro
 	brew install signal
 	brew install watchman
 	brew install nvim
 	brew install php
-	brew install chatgpt
-	brew install macs-fan-control
-	brew install zen-browser
-	brew install rust
-	brew install alacritty
-	brew install kitty
-	brew install rustdesk
 	brew install ranger
-	brew install libreoffice
 	brew install tree
 	brew install mpv
 	brew install utm
