bc71f5f43589c18071433ef3897fa64f615f483d
senalbulumulle <senal.bulumulle@gmail.com>
2025-11-23 03:45:11 +1000

    updated

---
+++ hypr/install.sh
@@ -0,0 +1,44 @@
+## @docs: required dependencies to install
+sudo pacman -S wofi --noconfirm
+sudo pacman -S mako --noconfirm
+sudo pacman -S pcmanfm --noconfirm
+sudo pacman -S swaybg --noconfirm
+sudo pacman -S pipewire --noconfirm
+sudo pacman -S pipewire-pulse --noconfirm
+sudo pacman -S pipewire-alsa --noconfirm
+sudo pacman -S wireplumber --noconfirm
+
+sudo pacman -S hyprland --noconfirm
+sudo pacman -S polkit-kde-agent --noconfirm
+sudo pacman -S qt5-wayland --noconfirm
+sudo pacman -S qt6-wayland --noconfirm
+sudo pacman -S xdg-desktop-portal-hyprland --noconfirm
+sudo pacman -S mako --noconfirm
+sudo pacman -S wl-clipboard --noconfirm
+
+
+sudo pacman -S hyprland --noconfirm
+sudo pacman -S polkit-kde-agent --noconfirm
+sudo pacman -S qt5-wayland --noconfirm
+sudo pacman -S qt6-wayland --noconfirm
+sudo pacman -S xdg-desktop-portal-hyprland --noconfirm
+sudo pacman -S kitty --noconfirm
+sudo pacman -S wofi --noconfirm
+sudo pacman -S waybar --noconfirm
+sudo pacman -S mako --noconfirm
+sudo pacman -S grim --noconfirm
+sudo pacman -S slurp --noconfirm
+
+sudo pacman -S wl-clipboard --noconfirm
+sudo pacman -S thunar --noconfirm
+sudo pacman -S network-manager-applet --noconfirm
+sudo pacman -S pavucontrol --noconfirm
+sudo pacman -S swaybg --noconfirm
+sudo pacman -S pipewire --noconfirm
+sudo pacman -S pipewire-pulse --noconfirm
+sudo pacman -S pipewire-alsa --noconfirm
+sudo pacman -S wireplumber --noconfirm
+
+
+
+
