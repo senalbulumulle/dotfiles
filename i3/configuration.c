588988d7b4fe0c6a0c3a81410e25e84f01db0f7d
senalbulumulle <senal.bulumulle@gmail.com>
2025-11-23 03:59:50 +1000

    updated

---
+++ i3/configuration.c
@@ -0,0 +1,12 @@
+// configuration.c
+//
+
+#include <stdio.h>			// include standard library
+#include <stdlib.h>			// include standard library
+#include <string.h>			// include standard library
+
+
+int main()
+{
+	system("echo 'hello'");
+}
---
+++ i3/install.sh
@@ -0,0 +1,9 @@
+## @docs: required dependencies to install
+sudo pacman -S pcmanfm --noconfirm
+sudo pacman -S network-manager-applet --noconfirm
+sudo pacman -S i3 --noconfirm
+sudo pacman -S i3lock --noconfirm
+sudo pacman -S i3status-rust --noconfirm
+sudo pacman -S i3blocks --noconfirm
+sudo pacman -S i3status --noconfirm
+sudo pacman -S i3-wm --noconfirm 
