f7a71a6a7a0d2bc473dcaec6d785c0f82afb84e4
senalbulumulle <senal.bulumulle@gmail.com>
2026-01-06 13:10:50 -0500

    That might work

--- hypr/configuration.c
+++ hypr/configuration.c
@@ -1,12 +1,24 @@
 // configuration.c
 //
 
-#include <stdio.h>			// include standard library
-#include <stdlib.h>			// include standard library
-#include <string.h>			// include standard library
+#include <stdio.h>			
+#include <stdlib.h>			
+#include <string.h>			
+#include <math.h>                       
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <iso646.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <unistd.h>      
+#include <time.h>        
+#include <assert.h>      
+
 
 
 int main()
 {
 	system("echo 'hello'");
-}
+} 
