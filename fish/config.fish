886f8cd84f59f119b1f60df80fb3d7fdff257d78
senalbulumulle <senal.bulumulle@gmail.com>
2026-01-13 05:16:06 -0800

    updated fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,3 @@
-## Senal's Fish config for macOS and Linux 
-
 if status is-interactive
-    alias microconfig='micro $HOME/.config/micro/settings.json'
-    alias vimconfig='vim $HOME/.vimrc'
-    alias bashconfig='micro $HOME/.bashrc'
-    alias fishconfig='micro $HOME/.config/fish/config.fish'
-    alias zshconfig='micro $HOME/.zshrc'
-    alias neovideconfig='echo "not available yet"'
-    alias v='nvim'
-    alias m='micro'
-    alias lsl='tree'
-    alias markdown='open /Applications/MarkText.app/'
-    alias createtemplate='sh scripts/create-template.sh'
-    alias organizefbext='sh scripts/01-organize-folders-file-extensions.sh'
-    alias lvim='/Users/senal/.local/bin/lvim'
+    # Commands to run in interactive sessions can go here
 end
