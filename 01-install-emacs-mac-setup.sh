89d17ce24c79bfc45910c483d58a87dffaf65d06
senalbulumulle <senal.bulumulle@gmail.com>
2026-02-04 14:58:41 -0500

    removed

--- 01-install-emacs-mac-setup.sh
+++
@@ -1,4 +0,0 @@
-## 01-install-emacs-mac-setup.sh
-brew install --cask emacs
-echo "for now this feature in dotfiles is unavailable\n"
-echo "until further updates on the dotfiles"
--- 02-install-cachyos-arch-linux-setup.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -Syyu --noconfirm           ## upgrades all the packages
-sudo pacman -S yay --noconfirm          ## install the yay package
-
