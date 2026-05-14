d3a63375a99ea912658b28594733d0fcd8bef4a9
senal.bulumulle <senal.bulumulle@gmail.com>
2026-05-14 13:52:50 -0700

    .

--- sync-notes.sh
+++
@@ -1,9 +0,0 @@
-## sync-org-notes.sh
-
-## This script allows the ability to sync notes to the devices
-
-
-clear
-echo "Pick Devices to Sync To:\n"
-echo "1) ThinkPad T480:\n"
-echo "2) ReMarkable Paper Pro:\n"
