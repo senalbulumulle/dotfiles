dc6d009551b2e373e3db4a1f6e3d0d7b91cfbff4
senalbulumulle <senal.bulumulle@gmail.com>
2026-06-15 05:02:36 -0700

    added the macboook pro setup script

---
+++ 05-macbook-setup.sh
@@ -0,0 +1,18 @@
+## macbook-setup.sh
+
+
+## This script is the latest one, which sets up everything on my
+## macboooook proooooo
+
+brew install kitty
+brew install alacritty
+brew install obsidian
+brew install telegram
+brew install puremac
+brew install google-chrome
+brew install loop
+brew install zed
+brew install cursor
+brew install vscodium
+brew install zoom
+
