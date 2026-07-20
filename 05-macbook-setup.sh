1015f3f431a712cb0a965da0be9b80af2977bc42
senalbulumulle <senal.bulumulle@gmail.com>
2026-07-20 05:54:59 -0700

    added more installations to the 05-macbook-setup.sh script

--- 05-macbook-setup.sh
+++ 05-macbook-setup.sh
@@ -4,16 +4,144 @@
 ## This script is the latest one, which sets up everything on my
 ## macboooook proooooo
 
-brew install kitty
-brew install alacritty
-brew install obsidian
-brew install telegram
-brew install puremac
-brew install google-chrome
-brew install loop
-brew install zed
-brew install cursor
-brew install vscodium
-brew install zoom
-brew install appcleaner
+## Taps
+brew tap felixkratz/formulae
+
+## CLI tools / formulae
+brew install automake
+brew install azure-cli
+brew install bash
+brew install binutils
+brew install bitcoin
+brew install cglm
+brew install cjson
+brew install cmake
+brew install colima
+brew install conan
+brew install csfml
+brew install cvs
+brew install docker
+brew install doxygen
+brew install dune
+brew install emacs
+brew install exiftool
+brew install fd
+brew install fish
+brew install flex
+brew install foremost
+brew install freeglut
+brew install fswatch
+brew install gh
+brew install git
+brew install git-lfs
+brew install glew
+brew install glfw
+brew install glow
+brew install glyph
+brew install groff
+brew install gtk+3
+brew install helix
+brew install htop
+brew install i686-elf-gcc
+brew install i686-elf-grub
+brew install jansson
+brew install javacc
+brew install kubo
+brew install libev
+brew install libgcrypt
+brew install librsvg
+brew install lima-additional-guestagents
+brew install luarocks
+brew install mas
+brew install meson
+brew install micro
+brew install mpv
+brew install nano
+brew install nasm
+brew install neovim
+brew install nextdns
+brew install nushell
+brew install ocaml
+brew install openssh
+brew install pcp
+brew install php
+brew install platformio
+brew install podman
+brew install protobuf
+brew install pybind11
+brew install python-tk@3.14
+brew install ranger
+brew install raylib
+brew install rust
+brew install scipy
+brew install scrcpy
+brew install sdl2_gfx
+brew install sdl2_image
+brew install sdl2_mixer
+brew install sdl2_ttf
+brew install solidity
+brew install stellar-core
+brew install stow
+brew install tccutil
+brew install tectonic
+brew install testdisk
+brew install tmux
+brew install tree
+brew install truffle
+brew install uv
+brew install vim
+brew install watchman
+brew install wget
+brew install x86_64-elf-gcc
+brew install x86_64-elf-grub
+brew install xmrig
+brew install xterm
+brew install yajl
+brew install zellij
+brew install zig
 brew install sketchybar
+
+## GUI apps / casks
+brew install --cask alacritty
+brew install --cask android-platform-tools
+brew install --cask appcleaner
+brew install --cask blackhole-2ch
+brew install --cask brave-browser
+brew install --cask brave-browser@nightly
+brew install --cask chatgpt
+brew install --cask claude-code
+brew install --cask codex
+brew install --cask cursor
+brew install --cask cursor-cli
+brew install --cask discord@canary
+brew install --cask emacs-app
+brew install --cask floorp
+brew install --cask font-hack-nerd-font
+brew install --cask ghostty
+brew install --cask google-chrome
+brew install --cask jdownloader
+brew install --cask kitty
+brew install --cask loop
+brew install --cask macs-fan-control
+brew install --cask microsoft-teams
+brew install --cask mochi-diffusion
+brew install --cask obsidian
+brew install --cask puremac
+brew install --cask rustdesk
+brew install --cask signal
+brew install --cask slack
+brew install --cask stats
+brew install --cask telegram
+brew install --cask tradingview
+brew install --cask utm
+brew install --cask visual-studio-code
+brew install --cask vscodium
+brew install --cask webull
+brew install --cask zed
+brew install --cask zoom
+
+## Mac App Store apps (requires `mas`, run `mas signin` first)
+mas install 1631624924 # Final Cut Pro
+mas install 361285480  # Keynote
+mas install 361309726  # Pages
+mas install 497799835  # Xcode
