f723caf173ec9e49f547b40cd285c1306d429186
senalbulumulle <senal.bulumulle@gmail.com>
2026-08-20 12:29:19 -0700

    fixed issues with keyring for installation of brave on debian

--- pc-1-debian/04-install-brave.sh
+++ pc-1-debian/04-install-brave.sh
@@ -10,7 +10,7 @@ sudo apt update
 sudo apt install -y curl
 
 # Add Brave's GPG key
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 # Add Brave repository
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
