fae631a2afdb8bb612dbddf013c27f182031e1aa
Senal <senal@Senals-MacBook-Pro.local>
2026-09-02 12:04:59 -0700

    updated

---
+++ macbook/02-install-packages.sh
@@ -0,0 +1,12 @@
+## 02-install-packages.sh
+
+brew install --cask gh
+brew install --cask cursor
+brew install --cask google-chrome
+brew install --cask brave-browser
+brew install --cask cursor
+brew install --cask telegram
+brew install --cask floorp
+brew install --cask emacs
+brew install --cask discord
+brew install --cask claude-code
